1b533efbb977c59f1657393950094bcf6c05a58f
mrshelly <mrshelly@401c260a-d80c-6a5c-8b96-3233dd31dd92>
2010-04-10 10:42:25 +0000

    多级菜单的实现.

--- trunk/www/tpl/products.php
+++ trunk/www/tpl/products.php
@@ -5,8 +5,14 @@
 
 			div.clear {clear:both;}
 		</style>
+
+		<link href="/res/css/dropdown.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="/res/css/default.ultimate.css" media="screen" rel="stylesheet" type="text/css" />
 		<script type="text/javascript" src="/res/js/jquery.js"></script>
 		<script type="text/javascript" src="/res/js/jquery.json.js"></script>
+		<!--[if lt IE 7]>
+		<script type="text/javascript" src="/res/js/jquery.dropdown.js"></script>
+		<![endif]-->
 	</head>
 	<body>
 		<div class="head">
@@ -18,11 +24,11 @@
 		</div>
 
 		<div class="menu">
-			<ul><?php
+			<ul id="nav" class="dropdown dropdown-horizontal"><?php
 				if(is_array($outArray['menu']) && count($outArray['menu'])>0){
-					foreach($outArray['menu'] as $k=>$v){?>
-
-				<li class="<?php echo $k; ?>"><a href="<?php echo $v['url']; ?>"><?php echo $v['name']; ?></a></li><?php
+					echo "\n";
+					foreach($outArray['menu'] as $k=>$v){
+						echo getNodeMenuHTML($outArray['menu'], $k, 3);
 					}
 				}?>
 
